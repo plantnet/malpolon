8407d13d03b6cbd32d0461bb46e99a84c127c826
LORIEUL Titouan <titouan.lorieul@inria.fr>
2022-06-14 13:58:26 +0200

    Updated GeoLifeCLEF 2022 example

--- examples/geolifeclef/cnn_on_rgb_patches.py
+++ examples/geolifeclef/cnn_on_rgb_patches.py
@@ -1,36 +1,38 @@
-import argparse
-from typing import Optional
+from argparse import Namespace
 
+import hydra
+from omegaconf import DictConfig
 import pytorch_lightning as pl
+import torchmetrics.functional as Fmetrics
 from torchvision import transforms
 
-from ...data_module import BaseDataModule
-from ...datasets.geolifeclef import GeoLifeCLEF2022Dataset
-from ...model import StandardClassificationSystem
+from malpolon.data.data_module import BaseDataModule
+from malpolon.data.datasets.geolifeclef import GeoLifeCLEF2022Dataset, MiniGeoLifeCLEF2022Dataset
+from malpolon.models.standard_classification_models import StandardFinetuningClassificationSystem
 
 
-class RGBPatchesGeoLifeCLEF22DataModule(BaseDataModule):
+class GeoLifeCLEF2022DataModule(BaseDataModule):
+    r"""
+    Data module for GeoLifeCLEF 2022.
+
+    Parameters
+    ----------
+        dataset_path: Path to dataset
+        train_batch_size: Size of batch for training
+        inference_batch_size: Size of batch for inference (validation, testing, prediction)
+        num_workers: Number of workers to use for data loading
+    """
     def __init__(
         self,
         dataset_path: str,
-        train_batch_size: Optional[int] = 64,
-        inference_batch_size: Optional[int] = 256,
-        num_workers: Optional[int] = 8,
+        minigeolifeclef: bool = False,
+        train_batch_size: int = 32,
+        inference_batch_size: int = 256,
+        num_workers: int = 8,
     ):
-        r"""Data module for GeoLifeCLEF22.
-
-        Args:
-            dataset_path: Path to dataset
-            train_batch_size: Size of batch for training
-            inference_batch_size: Size of batch for inference (validation, testing, prediction)
-            num_workers: Number of workers to use for data loading
-        """
-        super().__init__(
-            train_batch_size,
-            inference_batch_size,
-            num_workers,
-        )
+        super().__init__(train_batch_size, inference_batch_size, num_workers)
         self.dataset_path = dataset_path
+        self.minigeolifeclef = minigeolifeclef
 
     @property
     def train_transform(self):
@@ -62,7 +64,12 @@ class RGBPatchesGeoLifeCLEF22DataModule(BaseDataModule):
         )
 
     def get_dataset(self, split, transform, **kwargs):
-        dataset = GeoLifeCLEF2022Dataset(
+        if self.minigeolifeclef:
+            dataset_cls = MiniGeoLifeCLEF2022Dataset
+        else:
+            dataset_cls = GeoLifeCLEF2022Dataset
+
+        dataset = dataset_cls(
             self.dataset_path,
             split,
             patch_data=["rgb"],
@@ -73,106 +80,43 @@ class RGBPatchesGeoLifeCLEF22DataModule(BaseDataModule):
         return dataset
 
 
-class ReplaceBlueChannelByNearIRTransform:
-    def __call__(self, data):
-        rgb, near_ir = data
-        rgb[:, :, 2] = near_ir
-        return rgb
-
-
-class RGNIRPatchesGeoLifeCLEF22DataModule(RGBPatchesGeoLifeCLEF22DataModule):
-    @property
-    def train_transform(self):
-        return transforms.Compose(
-            [
-                ReplaceBlueChannelByNearIRTransform(),
-                super().train_transform
-            ]
-        )
-
-    @property
-    def test_transform(self):
-        return transforms.Compose(
-            [
-                ReplaceBlueChannelByNearIRTransform(),
-                super().test_transform
-            ]
-        )
-
-    def get_dataset(self, split, transform, **kwargs):
-        dataset = GeoLifeCLEF2022Dataset(
-            self.dataset_path,
-            split,
-            patch_data=["rgb", "near_ir"],
-            use_rasters=False,
-            transform=transform,
-            **kwargs
-        )
-        return dataset
-
-
-class ClassificationSystem(StandardClassificationSystem):
+class ClassificationSystem(StandardFinetuningClassificationSystem):
     def __init__(
         self,
         model_name: str = "resnet18",
+        num_classes: int = 100,
         pretrained: bool = True,
         lr: float = 1e-2,
         weight_decay: float = 0,
-        num_classes: int = 17037,
+        momentum: float = 0.9,
+        nesterov: bool = True,
     ):
-        metrics = {
+        super().__init__(model_name, num_classes, pretrained, lr, weight_decay, momentum, nesterov)
+
+        self.metrics = {
             "accuracy": Fmetrics.accuracy,
             "top_k_accuracy": lambda y_hat, y: Fmetrics.accuracy(y_hat, y, top_k=30),
         }
-        super().__init__(
-            model_name,
-            pretrained,
-            num_classes,
-            lr=lr,
-            weight_decay=weight_decay,
-            metrics=metrics,
-        )
 
 
-if __name__ == "__main__":
+@hydra.main(config_path=".", config_name="cnn_on_rgb_patches_config")
+def main(cls: DictConfig):
     """
-    from pytorch_lightning.utilities.cli import LightningArgumentParser
-    parser = LightningArgumentParser()
-    parser.add_lightning_class_args(Model, "model")
-    parser.add_lightning_class_args(GeoLifeCLEF22DataModule, "data")
-    """
-    parser = argparse.ArgumentParser(
-        description="Example of training and testing on GeoLifeCLEF2022."
-    )
-    parser = RGBPatchesGeoLifeCLEF22DataModule.add_argparse_args(parser, use_argument_group=False)
-    parser = ClassificationSystem.add_argparse_args(parser, use_argument_group=False)
-    parser = pl.Trainer.add_argparse_args(parser)
-    parser.add_argument(
-        "--data",
-        choices=["rgb", "rg-nir"],
-    )
-    """
-    parser.add_argument(
-        "--max_epochs",
-        type=int,
-        help="number of training epochs",
-    )
-    """
-    args = parser.parse_args()
-
     logger = pl.loggers.CSVLogger("logs")
     logger.log_hyperparams(args)
+    """
+    logger = None
 
-    if args.data == "rgb":
-        datamodule = RGBPatchesGeoLifeCLEF22DataModule.from_argparse_args(args)
-    else:
-        datamodule = RGNIRPatchesGeoLifeCLEF22DataModule.from_argparse_args(args)
+    datamodule = GeoLifeCLEF2022DataModule.from_argparse_args(Namespace(**cls.data))
 
-    model = ClassificationSystem.from_argparse_args(args)
+    model = ClassificationSystem.from_argparse_args(Namespace(**cls.model))
     print(model.model)
 
-    #trainer = pl.Trainer(gpus=1, max_epochs=args.max_epochs, logger=logger)
-    trainer = pl.Trainer.from_argparse_args(args, gpus=1, logger=logger)
+    trainer = pl.Trainer(gpus=1, logger=logger, **cls.params)
     trainer.fit(model, datamodule=datamodule)
 
     trainer.test(model, datamodule=datamodule)
+
+
+if __name__ == "__main__":
+    main()
