c8674b204e2a95dcc4f56d302ea7e093869c4afc
tlarcher <larchertheo@gmail.com>
2023-06-26 12:15:08 +0200

    Updated WiP. Remaining to do: implement both samplers strategies to RasterTorchGeoDataset (one randomgeosampler, one custom with labels and int indexing

--- malpolon/tests/test_environmental_raster.py
+++ malpolon/tests/test_environmental_raster.py
@@ -2,15 +2,38 @@ from pathlib import Path
 
 import numpy as np
 import pytest
+import torch
 
-from malpolon.data.environmental_raster import PatchExtractor
+from malpolon.data.environmental_raster import PatchExtractor, RasterTorchGeo
 
-
-DATA_PATH = Path("<path to GeoLifeClef data>")
-
+DATA_PATH = Path("malpolon/tests/")
 
 def test_patch_query_torchgeo():
-    return None
+    class Sentinel2(RasterTorchGeo):
+        filename_glob = DATA_PATH / "torchgeo_sentinel2_test.tif"
+        is_image = True
+        separate_files = True
+        all_bands = ["B08"]
+        rgb_bands = ["B08"]
+    class MicroLifeClef(RasterTorchGeo):
+        filename_glob = DATA_PATH / "torchgeo_mlc_test.tif"
+        is_image = True
+        separate_files = True
+        all_bands = ["bio_1"]
+        rgb_bands = ["bio_1"]
+    dataset_s2, dataset_mlc = Sentinel2('./'), MicroLifeClef('./')
+    patch1_s2 = dataset_s2[{'lon':3.87075, 'lat':43.61135, 'crs':dataset_s2.crs_pyproj.geodetic_crs, 'units':'pixel', 'size': (100, 100)}]['image'][0]
+    patch2_s2 = dataset_s2[{'lon':570265.8337376957, 'lat':4829076.115471331, 'crs':dataset_s2.crs_pyproj, 'units':'m', 'size': 1000}]['image'][0]
+    patch_mlc = dataset_mlc[{'lon':3.87075, 'lat':43.61135, 'crs':dataset_mlc.crs_pyproj, 'units':'m', 'size': (930*0.7142857142857143*100, 930*100)}]['image'][0]
+    expected_patch_s2 = torch.load(DATA_PATH / 'torchgeo_sentinel2_expected.raw')
+    expected_patch_mlc = torch.load(DATA_PATH / 'torchgeo_mlc_expected.raw')
+    assert tuple(patch1_s2.shape) == expected_patch_s2.shape
+    assert tuple(patch2_s2.shape) == expected_patch_s2.shape
+    assert tuple(patch_mlc.shape) == expected_patch_mlc.shape
+    np.testing.assert_allclose(patch1_s2, expected_patch_s2)
+    np.testing.assert_allclose(patch2_s2, expected_patch_s2)
+    np.testing.assert_allclose(patch_mlc, expected_patch_mlc)
+
 
 def test_patch_extractor_single_variable():
     extractor = PatchExtractor(DATA_PATH / "rasters", size=8)
