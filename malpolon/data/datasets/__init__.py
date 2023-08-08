e4c971a698b4b7d454864ffbfae799a0ba54110d
tlarcher <larchertheo@gmail.com>
2023-08-08 11:35:55 +0200

    Added raster methods to data.datasets.__init__

--- malpolon/data/datasets/__init__.py
+++ malpolon/data/datasets/__init__.py
@@ -1,7 +1,10 @@
 from .geolifeclef2022 import GeoLifeCLEF2022Dataset, MiniGeoLifeCLEF2022Dataset
+from .torchgeo_datasets import RasterTorchGeoDataset, RasterSentinel2
 
 
 __all__ = [
     "GeoLifeCLEF2022Dataset",
     "MiniGeoLifeCLEF2022Dataset",
+    "RasterTorchGeoDataset",
+    "RasterSentinel2"
 ]
