Fix crs units (#30)
* Fixed value of crs passed to sentinel2 sampler in inference mode byt creating a new sampler in the example file, inheriting Sentinel2GeoSampler.
Updated main README.

* WiP.
Somehow in inference mode with .predict(), the datamodule doesn't uses its own test_dataloader cause default arguments of sentinel2geosampler are used.

* Fixed CRS out-of-bounds error on inference mode when calling model_loaded.predict().
Modified predict behavior to always take the config file's units and CRS arguments as parameter for the sampler.
Updated the docstrings and the Sentinel2 README to correctly describe the crs and units arguments

malpolon/models/standard_prediction_systems.py
@@ -183,7 +183,7 @@ class GenericPredictionSystem(pl.LightningModule):
             Predicted tensor values.
         """
         datamodule.setup(stage="test")
-        predictions = trainer.predict(datamodule=datamodule, model=self)
+        predictions = trainer.predict(dataloaders=datamodule.test_dataloader(), model=self)
         predictions = torch.cat(predictions)
         return predictions
 