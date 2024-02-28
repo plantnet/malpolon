ea68216b0f123647422959749d4b21ec0f7beb89
Theo Larcher <42494948+tlarcher@users.noreply.github.com>
2024-02-28 20:21:56 +0100

    Malpolon v1.0.2 Sphinx documentation (#46)
    * Update README.md
    
    * Updated and re-structured Malpolon's Sphinx documentation.
    - Updated some docstrings to satifsy Sphinx import requirements
    - Renamed sentinel-2a directoris to sentinel_2a to satisfy Sphinx reference requirements
    - Updated __init__.py files
    - Updated inference/sentinel_2a config file with non-absolute path to checkpoint weights
    - Fixed broken symbolic links
    
    * Updated Sphinx doc with more consive welmcome page

--- malpolon/data/__init__.py
+++ malpolon/data/__init__.py
@@ -1,6 +1,16 @@
 from .data_module import BaseDataModule
-
+from .environmental_raster import PatchExtractor, Raster
+from .get_jpeg_patches_stats import standardize
+from .utils import (get_files_path_recursively, is_bbox_contained,
+                    is_point_in_bbox, to_one_hot_encoding)
 
 __all__ = [
     "BaseDataModule",
+    "PatchExtractor",
+    "Raster",
+    "standardize",
+    "get_files_path_recursively",
+    "is_bbox_contained",
+    "is_point_in_bbox",
+    "to_one_hot_encoding"
 ]
