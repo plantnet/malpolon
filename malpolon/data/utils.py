3f67f0350d4f0ee49ae71d8d6a9ec943bac0ec73
aerodynamic-sauce-pan <larchertheo@gmail.com>
2024-05-31 19:19:02 +0200

    Renamed scripts. - malpolon/data/utils.py: Added scripts/split_obs_spatially.py script as malpolon callable utils method

--- malpolon/data/utils.py
+++ malpolon/data/utils.py
@@ -10,8 +10,12 @@ import re
 from typing import Iterable, Union
 
 import numpy as np
+import pandas as pd
 from shapely import Point, Polygon
 from torchgeo.datasets import BoundingBox
+from verde import train_test_split as spatial_tts
+
+from malpolon.plot.map import plot_observation_dataset as plot_od
 
 
 def is_bbox_contained(
@@ -155,3 +159,50 @@ def get_files_path_recursively(path, *args, suffix='') -> list:
               for f in filenames
               if re.search(rf"^.*({suffix})\.({ext_list})$", f)]
     return result
+
+
+def split_obs_spatially(input_name: str,
+                        spacing: float = 10 / 60,
+                        plot: bool = False,
+                        val_size: float = 0.15):
+    """Perform a spatial train/val split on the input csv file.
+
+    Parameters
+    ----------
+    input_name : str
+        obs CSV input file's name without the .csv extension.
+    spacing : float, optional
+        size of the spatial split in degrees (or whatever unit the coordinates are in),
+        by default 10/60
+    plot : bool, optional
+        if true, plots the train/val split on a 2D map,
+        by default False
+    val_size : float, optional
+        size of the validaiton split, by default 0.15
+    """
+    df = pd.read_csv(f'{input_name}.csv')
+    coords, data = {}, {}
+    for col in df.columns:
+        if col in ['lon', 'lat']:
+            coords[col] = df[col].to_numpy()
+        else:
+            data[col] = df[col].to_numpy()
+    train_split, val_split = spatial_tts(tuple(coords.values()), tuple(data.values()),
+                                         spacing=spacing, test_size=val_size)
+
+    df_train = pd.DataFrame({'lon': train_split[0][0], 'lat': train_split[0][1]})
+    df_val = pd.DataFrame({'lon': val_split[0][0], 'lat': val_split[0][1]})
+    df_train['subset'] = ['train'] * len(df_train)
+    df_val['subset'] = ['val'] * len(df_val)
+    for train_data, val_data, col in zip(train_split[1], val_split[1], data.keys()):
+        df_train[col] = train_data
+        df_val[col] = val_data
+
+    df_train_val = pd.concat([df_train, df_val])
+
+    df_train_val.to_csv(f'{input_name}_train_val-{spacing*60}min.csv', index=False)
+    df_train.to_csv(f'{input_name}_train-{spacing*60}min.csv', index=False)
+    df_val.to_csv(f'{input_name}_val-{spacing*60}min.csv', index=False)
+
+    if plot:
+        plot_od(df=df_train_val, show_map=True)
