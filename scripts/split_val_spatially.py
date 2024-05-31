f3be50fe780c9eea17beb8a3d686fd9e070ec2d0
aerodynamic-sauce-pan <larchertheo@gmail.com>
2024-05-31 15:40:47 +0200

    Added new script to perform CSV obs spatial split (train / val)  based on lon/lat coordinates, using the Verde package.

---
+++ scripts/split_val_spatially.py
@@ -0,0 +1,65 @@
+"""This script splits an obs csv in val/train based on the observations'
+geogrpahic locations using the Verde package.
+
+Author: Théo Larcher <theo.larcher@inria.fr>
+"""
+
+import argparse
+
+import pandas as pd
+from verde import train_test_split as spatial_tts
+
+from malpolon.plot.map import plot_observation_dataset as plot_od
+
+
+def main(input_name: str,
+         spacing: float = 20 / 60,
+         plot: bool = False,
+         val_size: float = 0.15):
+    df = pd.read_csv(f'{input_name}.csv')
+    coords, data = {}, {}
+    for col in df.columns:
+        if col in ['lon', 'lat']:
+            coords[col] = df[col].to_numpy()
+        else:
+            data[col] = df[col].to_numpy()
+    train_split, val_split = spatial_tts(tuple(coords.values()), tuple(data.values()),
+                                         spacing=spacing, test_size=val_size)
+
+    df_train = pd.DataFrame({'lon': train_split[0][0], 'lat': train_split[0][1]})
+    df_val = pd.DataFrame({'lon': val_split[0][0], 'lat': val_split[0][1]})
+    df_train['subset'] = ['train'] * len(df_train)
+    df_val['subset'] = ['val'] * len(df_val)
+    for train_data, val_data, col in zip(train_split[1], val_split[1], data.keys()):
+        df_train[col] = train_data
+        df_val[col] = val_data
+
+    df_train_val = pd.concat([df_train, df_val])
+
+    df_train_val.to_csv(f'{input_name}_train_val-{spacing*60}min.csv', index=False)
+    df_train.to_csv(f'{input_name}_train-{spacing*60}min.csv', index=False)
+    df_val.to_csv(f'{input_name}_val-{spacing*60}min.csv', index=False)
+
+    if plot:
+        plot_od(df=df_train_val, show_map=True)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input_name",
+                        help="Name of the input csv obs file without the .csv extension",
+                        default='GLC24_PA_metadata_train',
+                        type=str)
+    parser.add_argument("-s", "--spacing",
+                        help="Size of the spatial split in degrees (or whatever unit the coordinates are in)",
+                        default=10/60,
+                        type=float)
+    parser.add_argument("--val_size",
+                        help="Size of the validation subset to produce.",
+                        default=0.15,
+                        type=float)
+    parser.add_argument("-p", "--plot",
+                        help="If true, plot the train/val split at the end of the script.",
+                        action='store_true')
+    args = parser.parse_args()
+    main(args.input_name, args.spacing, plot=args.plot, val_size=args.val_size)
