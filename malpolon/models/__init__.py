522b236ce6772916e79bb2f99e95633d3c0e956a
aerodynamic-sauce-pan <larchertheo@gmail.com>
2024-07-30 14:53:16 +0200

    Fixed models import

--- malpolon/models/__init__.py
+++ malpolon/models/__init__.py
@@ -1,7 +1,6 @@
 from .standard_prediction_systems import *  # noqa: F403
 
-
 __all__ = [  # noqa: F405
     "GenericPredictionSystem",
-    "FinetuningClassificationSystem",
+    "ClassificationSystem",
 ]
