4574e1ab87eaddbadabc7c1756073957e63019dd
Auguste MARBEC <augusteverdier@orange.fr>
2024-07-31 20:13:16 +0200

    write LightningDataModule

--- examples/poverty/datamodule/landsat_poverty.py
+++ examples/poverty/datamodule/landsat_poverty.py
@@ -2,16 +2,17 @@ import torch
 import os
 import numpy as np
 from torch.utils.data import Dataset
-from torchgeo.datasets.utils import BoundingBox
 import torchvision
 import pickle
 
-from malpolon.data.data_module import BaseDataModule
-from torchgeo.datasets import  RasterDataset
+import pandas as pd
 
+import pytorch_lightning as pl
+from torch.utils.data import DataLoader, random_split
+from torchvision import transforms
 import rasterio
 
-# TODO : Change gdal to read .tiff files
+# TODO : Add JITTER and NORMALIZER to transfomer LightningDataModule, top remove ``preprocess_landsat`` step
 
 
 NORMALIZER = 'datasets/normalizer.pkl'
@@ -31,7 +32,7 @@ DESCRIPTOR = {
     'NIGHTLIGHTS': "float"
 }
 
-JITTER = torchvision.transforms.ColorJitter(brightness=0.1, contrast=0.1)
+JITTER =transforms.ColorJitter(brightness=0.1, contrast=0.1)
 
 
 def preprocess_landsat(raster, normalizer, jitter=None):
@@ -52,10 +53,36 @@ def preprocess_landsat(raster, normalizer, jitter=None):
 
 
 
+class RegressionDataModule(pl.LightningDataModule):
+    def __init__(self, csv_file, tif_dir, batch_size=32, transform=None, val_split=0.2):
+        super().__init__()
+        self.dataframe = pd.read_csv(csv_file)
+        self.tif_dir = tif_dir
+        self.batch_size = batch_size
+        self.transform = torch.nn.Sequential(
+            torchvision.transforms.CenterCrop(224),
+            torchvision.transforms.RandomHorizontalFlip(),
+            torchvision.transforms.RandomVerticalFlip()
+        )
+        self.val_split = val_split
+
+    def setup(self, stage=None):
+        full_dataset = MSDataset(self.dataframe, self.tif_dir, transform=self.transform)
+        
+        val_size = int(len(full_dataset) * self.val_split)
+        train_size = len(full_dataset) - val_size
+        self.train_dataset, self.val_dataset = random_split(full_dataset, [train_size, val_size])
+
+    def train_dataloader(self):
+        return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True)
+
+    def val_dataloader(self):
+        return DataLoader(self.val_dataset, batch_size=self.batch_size)
+
 
 class MSDataset(Dataset):
 
-    def __init__(self, dataframe, root_dir, normalizer=NORMALIZER, test_flag=False):
+    def __init__(self, dataframe, root_dir, normalizer=NORMALIZER):
         """
         Args:
             dataframe (Pandas DataFrame): Pandas DataFrame containing image file names and labels.
@@ -65,7 +92,6 @@ class MSDataset(Dataset):
         self.root_dir = root_dir
         with open(normalizer, 'rb') as f:
             self.normalizer = pickle.load(f)
-        self.test_flag = test_flag
 
     def __len__(self):
         return len(self.dataframe)
@@ -74,7 +100,6 @@ class MSDataset(Dataset):
         if torch.is_tensor(idx):
             idx = idx.tolist()
 
-        
         row = self.dataframe.iloc[idx]
 
         value = row.wealthpooled.astype('float')
@@ -84,7 +109,6 @@ class MSDataset(Dataset):
                                  str(row.cluster) + ".tif"
                                  )
 
-        
         tile = np.empty([7, 255, 255])
 
         with rasterio.open(tile_name) as src:
@@ -92,16 +116,19 @@ class MSDataset(Dataset):
             for band in src.indexes[0:-1]:
                 tile[band-1, :, :] = src.read(band)
 
-        tile = torch.from_numpy(np.nan_to_num(tile))
+        tile = np.nan_to_num(tile)
 
-        
-        transforms = torch.nn.Sequential(
-            torchvision.transforms.CenterCrop(224),
-            torchvision.transforms.RandomHorizontalFlip(),
-            torchvision.transforms.RandomVerticalFlip()
-        )
+        # tile = preprocess_landsat(tile, self.normalizer['landsat_+_nightlights'], JITTER)
 
-        tile = transforms(tile)
+        return torch.tensor(tile, dtype=torch.float32), torch.tensor(value, dtype=torch.float32)
+    
 
-        tile = preprocess_landsat(tile, self.normalizer['landsat_+_nightlights'], JITTER)
-        return tile, value
+if __name__ == "__main__":
+
+    dm = RegressionDataModule("datasets/landsat_poverty.csv", "datasets/landsat_tiles")
+    dm.setup()
+    dl = dm.train_dataloader()
+    for i, (x, y) in enumerate(dl):
+        print(x.shape, y.shape)
+        if i > 3:
+            break
