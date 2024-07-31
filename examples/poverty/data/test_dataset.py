f44627614863ec1031cd1d7baeb0cc82cfa275eb
Auguste Verdier <augusteverdier@orange.fr>
2024-07-31 18:09:48 +0200

    display poverty images / bands

--- examples/poverty/data/test_dataset.py
+++ examples/poverty/data/test_dataset.py
@@ -10,25 +10,18 @@ from malpolon.data.data_module import BaseDataModule
 from torchgeo.datasets import  RasterDataset
 
 import rasterio
+from matplotlib import pyplot
+import random
 
 
 class CustomTiffDataset(RasterDataset):
-    def __init__(self, root: str, transforms=None):
-        super().__init__(transforms=transforms)
-        self.root = root
-        self.files = list(self.root.glob("*.tiff"))  # Assuming all .tiff files are in the root directory
-
-    def __getitem__(self, index: int):
-        filepath = self.files[index]
-        with rasterio.open(filepath) as src:
-            image = src.read()  # Reads the image as a NumPy array
-            image = torch.from_numpy(image).float()  # Convert to torch tensor
-            if self.transforms:
-                image = self.transforms(image)
-        return image
-
-    def __len__(self):
-        return len(self.files)
+    filename_glob = "*.tif"
+    # filename_regex = r"^.{6}_(?P<date>\d{8}T\d{6})_(?P<band>B0[\d])"
+    # date_format = "%Y%m%dT%H%M%S"
+    is_image = True
+    separate_files = True
+    all_bands = BANDS = ['BLUE', 'GREEN', 'RED', 'NIR', 'SWIR1', 'SWIR2', 'TEMP1']
+    rgb_bands = ['BLUE', 'GREEN', 'RED']
     
     
     
@@ -41,3 +34,32 @@ class PovertyTiffDataset(RasterDataset):
 class PovertyDataModule(BaseDataModule):
     def __init__():
         pass
+
+
+if __name__ == "__main__":
+
+    
+    root="examples/poverty/data/landsat_7_less/angola_2015/"
+    tile = np.empty([8, 255, 255])
+    
+
+    files = os.listdir(root)
+    
+    tif_files = [file for file in files if file.endswith('.tif')]
+    file = random.choice(tif_files)
+    with rasterio.open(root+file) as src:
+        
+        for band in src.indexes:
+            tile[band-1, :, :] = src.read(band)
+        
+
+    
+    fig, axs = pyplot.subplots(2, 4, figsize=(12, 6))
+
+    for i, ax in enumerate(axs.flat):
+        ax.imshow(tile[i, ...], cmap='pink')
+        ax.set_title(f"Band {i+1}")
+
+    pyplot.tight_layout()
+    pyplot.show()
+    
