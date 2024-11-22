05e8dbed11989b140f99b943f8330eb3c574bcf2
Theo Larcher <42494948+tlarcher@users.noreply.github.com>
2024-11-22 15:08:26 +0100

    Fix pretrained weights checkpoint_path and inference weight double download (#74)
    * Updated glc24_pre_extracted pretrained checkpoint with fixedj wiehgt_dir path
    
    * Updated all examples to prevent double downloading of model weights when using inference mode.
    
    * Added malpolon qr code

--- examples/benchmarks/geolifeclef/geolifeclef2023/cnn_on_rgbnir_glc23_patches.py
+++ examples/benchmarks/geolifeclef/geolifeclef2023/cnn_on_rgbnir_glc23_patches.py
@@ -168,6 +168,7 @@ def main(cfg: DictConfig) -> None:
     """
     # Loggers
     log_dir = hydra.core.hydra_config.HydraConfig.get().runtime.output_dir
+    log_dir = log_dir.split(hydra.utils.get_original_cwd())[1][1:]  # Transforming absolute path to relative path
     logger_csv = pl.loggers.CSVLogger(log_dir, name="", version="")
     logger_csv.log_hyperparams(cfg)
     logger_tb = pl.loggers.TensorBoardLogger(log_dir, name="tensorboard_logs", version="")
@@ -196,7 +197,8 @@ def main(cfg: DictConfig) -> None:
     if cfg.run.predict:
         model_loaded = ClassificationSystem.load_from_checkpoint(cfg.run.checkpoint_path,
                                                                  model=classif_system.model,
-                                                                 hparams_preprocess=False)
+                                                                 hparams_preprocess=False,
+                                                                 weights_dir=log_dir)
 
         # Option 1: Predict on the entire test dataset (Pytorch Lightning)
         predictions = model_loaded.predict(datamodule, trainer)
