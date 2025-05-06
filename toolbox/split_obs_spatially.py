3f54eb39b9c6fffb92bce4b0f26417fbf55fb315
tlarcher <theo.larcher@inria.fr>
2025-05-06 12:07:58 +0200

    Improved efficiency of spatial split

--- toolbox/split_obs_spatially.py
+++ toolbox/split_obs_spatially.py
@@ -15,7 +15,9 @@ from malpolon.plot.map import plot_observation_dataset as plot_od
 def main(input_path: str,
          spacing: float = 10 / 60,
          plot: bool = False,
-         val_size: float = 0.15):
+         val_size: float = 0.15,
+         col_lon: str = 'lon',
+         col_lat: str = 'lat'):
     """Perform a spatial train/val split on the input csv file.
 
     Parameters
@@ -35,21 +37,17 @@ def main(input_path: str,
     df = pd.read_csv(f'{input_name}.csv')
     coords, data = {}, {}
     for col in df.columns:
-        if col in ['lon', 'lat']:
+        if col in [col_lon, col_lat]:
             coords[col] = df[col].to_numpy()
         else:
             data[col] = df[col].to_numpy()
-    train_split, val_split = spatial_tts((coords['lon'], coords['lat']), tuple(data.values()),
+    train_split, val_split = spatial_tts((coords[col_lon], coords[col_lat]), tuple(data.values()),
                                          spacing=spacing, test_size=val_size)
 
-    df_train = pd.DataFrame({'lon': train_split[0][0], 'lat': train_split[0][1]})
-    df_val = pd.DataFrame({'lon': val_split[0][0], 'lat': val_split[0][1]})
+    df_train = pd.DataFrame(dict(zip(data.keys(), train_split[1])))
+    df_val = pd.DataFrame(dict(zip(data.keys(), val_split[1])))
     df_train['subset'] = ['train'] * len(df_train)
     df_val['subset'] = ['val'] * len(df_val)
-    for train_data, val_data, col in zip(train_split[1], val_split[1], data.keys()):
-        df_train[col] = train_data
-        df_val[col] = val_data
-
     df_train_val = pd.concat([df_train, df_val])
 
     df_train_val.to_csv(f'{input_name}_train_val-{spacing*60}min.csv', index=False)
@@ -74,8 +72,16 @@ if __name__ == '__main__':
                         help="Size of the validation subset to produce.",
                         default=0.15,
                         type=float)
+    parser.add_argument("--col_lon",
+                        help="Name of the longitude column.",
+                        default='lon',
+                        type=str)
+    parser.add_argument("--col_lat",
+                        help="Name of the latitude column.",
+                        default='lat',
+                        type=str)
     parser.add_argument("-p", "--plot",
                         help="If true, plot the train/val split at the end of the script.",
                         action='store_true')
     args = parser.parse_args()
-    main(args.input_path, args.spacing, plot=args.plot, val_size=args.val_size)
+    main(args.input_path, args.spacing, plot=args.plot, val_size=args.val_size, col_lon=args.col_lon, col_lat=args.col_lat)
