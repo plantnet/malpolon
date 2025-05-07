d469593c29ee485a2cfb7f9546bb9a2fcd6f68ba
tlarcher <theo.larcher@inria.fr>
2025-05-07 13:49:15 +0200

    Added dataset visualization tool and visuals. Updated Readmes

--- examples/custom_train/multiscale_jrc/dataset/scale_1_species/Viz_species_distrib.py
+++ examples/custom_train/multiscale_jrc/dataset/scale_1_species/Viz_species_distrib.py
@@ -1,13 +1,7 @@
-Pl@ntNet & Gbif illustrations of individual plant species go here.
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
 
-The metadata file to be used is the merge of the `multimedia.tsv` and `PN_gbif_France_2005-2025_illustrated.csv` extract, based on the column `gbifID`, filtered on the desired zone. The 1st file contains the urls to fetch images online; while the 2nd file contains the coordinates.
-
-The URLs to the images are under the column `identifier`.
-The file paths are formed by the `gbifID` as the images are dumped directly in the dataset folder.
-The GPS coordinates are retrieved from columns `['decimalLongitude', 'decimalLatitude']`.
-
-- **Species distrib viz**
-```python
 df = pd.read_csv('PN_gbif_France_2005-2025_illustrated_CBN-med_train_val-10.0min.csv')
 df_train = df[df['subset']=='train']
 df_val = df[df['subset']=='val']
@@ -16,20 +10,23 @@ u_train, c_train = np.unique(df_train['speciesKey'], return_counts=True)
 u_val, c_val = np.unique(df_val['speciesKey'], return_counts=True)
 u = np.union1d(u_train, u_val)
 
+print(f'Nb of unique speciesKey in Train: {len(u_train)}')
+print(f'Nb of unique speciesKey in Val: {len(u_val)}')
+
 # Extend both split unique counts to include the species which are missing
 c_train_extended = []
 for k, v in enumerate(u):
     if not v in u_train:
         c_train_extended.append(0)
     else:
-        c_train_extended.append(c_train[np.argwhere(u_train==v)])
+        c_train_extended.append(int(c_train[np.argwhere(u_train==v)][0][0]))
 c_train_extended = np.array(c_train_extended)
 c_val_extended = []
 for k, v in enumerate(u):
     if not v in u_val:
         c_val_extended.append(0)
     else:
-        c_val_extended.append(c_val[np.argwhere(u_val==v)])
+        c_val_extended.append(int(c_val[np.argwhere(u_val==v)][0][0]))
 c_val_extended = np.array(c_val_extended)
 
 # Prepare descending cumulative counts representation
@@ -39,12 +36,15 @@ args_c_val_extended_sort_desc = np.argsort(c_val_extended)[::-1]
 def plot_bars(x1, y1, x2, y2):
     plt.figure()
     plt.bar(x1, y1, label='train', color='blue', alpha=0.5)
-    plt.bar(x2, y2, label='val', color='orange', alpha=0.5)
-    plt.title('PN_gbif_France_2005-2025_illustrated train/val unique speciesKey distribution')
+    plt.bar(x2, y2, label='val', color='orange')
+    n_zeros_train = len(np.where(y1==0)[0])
+    n_zeros_val =len(np.where(y2==0)[0])
+    plt.title('PN_gbif_France_2005-2025_illustrated train/val unique speciesKey distribution.\n'+\
+             f'Nb of unique speciesKey in Train: {len(y1)-n_zeros_train}\n'+\
+             f'Nb of unique speciesKey in Val: {len(y2)-n_zeros_val}')
     plt.legend()
     plt.xlabel('Unique speciesKey')
     plt.ylabel('Counts')
     plt.show()
     
 plot_bars(np.arange(0, len(u), 1), c_train_extended[args_c_train_extended_sort_desc], np.arange(0, len(u), 1), c_val_extended[args_c_val_extended_sort_desc])
-```
